07d3889e5afaf07f9bf59834dacb3ddfc1b79296
SOHAM_17 <sohambasak17@gmail.com>
2026-01-07 22:46:05 +0530

    Initial commit

--- src/p1.java
+++ src/p1.java
@@ -1,2 +1,27 @@
 public class p1 {
+    static  boolean twoSum (int [] arr ,int target)
+    {
+        int n = arr.length;
+        for (int i=0 ;i<n;i++)
+        {
+            for (int j=i+1 ; j<n ;j++)
+            {
+                if (arr[i] + arr[j]== target)
+                {
+                    return true;
+                }
+            }
+        }
+        return  false;
+    }
+
+
+    public static void main(String[] args) {
+        int [] arr = {0,-1,2,-3,1};
+        int target = -2;
+
+        boolean i = twoSum(arr , target) ? true :false;
+        System.out.println(i);
+
+    }
 }
