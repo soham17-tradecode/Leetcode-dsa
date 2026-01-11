0235e4bb18fc808bd44db0ff14379f7766009647
SOHAM_17 <sohambasak17@gmail.com>
2026-01-11 22:21:40 +0530

    Initial commit

---
+++ src/p2.java
@@ -0,0 +1,41 @@
+public class p2 {
+    //rearrange 0 and 1
+    static void segregation (int [] arr)
+    {
+        int n = arr.length;
+        int i=0 ,k=0;
+        int j=n-1;
+
+        while(i<j)
+        {
+            if (arr[i]==0)
+            {
+                arr[k] =arr[i];
+                i++;
+                k++;
+            } else if (arr[i]==arr[j]) {
+                j--;
+
+            } else if (arr[i]!=arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+
+        }
+        for (int value : arr) {
+            System.out.println(value);
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int ar[]={1,1,1,1,0};
+        segregation(ar);
+
+    }
+}
