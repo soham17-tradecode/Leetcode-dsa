fb990e66b072f31c54c46a1ee9da09e7c0f26bfd
SOHAM_17 <sohambasak17@gmail.com>
2026-01-12 21:14:18 +0530

    Initial commit

---
+++ src/p3.java
@@ -0,0 +1,26 @@
+public class p3 {
+    static int removeDup(int[] arr) { //remove duplicate
+        int i = 0, unique = 1, j = 1;
+
+        int n = arr.length;
+        while (j < n) {
+            if (arr[j] == arr[j - 1]) {
+                j++;
+
+            } else {
+                arr[i + 1] = arr[j];
+                i++;
+                j++;
+                unique++;
+            }
+
+        }
+        return unique;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 1, 2, 2, 3, 31};
+        System.out.println(removeDup(arr));
+
+    }
+}
