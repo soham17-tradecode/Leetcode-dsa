b644ab01f549a89d8dc21e94ec6865b8982bf404
SOHAM_17 <sohambasak17@gmail.com>
2026-01-23 13:44:06 +0530

    Initial commit

--- src/MinSizeSubArraySum.java
+++ src/MinSizeSubArraySum.java
@@ -2,28 +2,30 @@ public class MinSizeSubArraySum {
     static int minimum(int[] arr, int target) {
         //very important sliding window  
         int low = 0;
-        int n = arr.length - 1;
+        int n = arr.length ;
         int res = Integer.MAX_VALUE;
         int sum = 0;
         for (int high = 0; high < n; high++) {
-            sum = sum + arr[high];
+            sum +=  arr[high];
             while (sum >= target) {
-                int len = high - low + 1;
-                res = Math.min(res, len);
-                sum = sum - arr[low];
+                if( high - low+1 < res)
+                {
+                    res = high - low+1;
+                }
+                sum -=  arr[low];
                 low++;
             }
         }
-//        return (res == Integer.MAX_VALUE) ? 0 : res;
-        return res;
+        return res != Integer.MAX_VALUE ? res : 0;
+//        return res ;
 
 
     }
 
     public static void main(String[] args) {
 
-        int[] ar = {1, 2, 4, 4};
-        int k = 4;
+        int[] ar = {2,3,1,2,4,3};
+        int k = 7;
         System.out.println(minimum(ar, k));
 
     }
---
+++ src/longestSubStringwithKDistinct.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class longestSubStringwithKDistinct {
+    static int LongestSubString(String arr, int k) {
+        int n = arr.length();
+        HashMap<Character, Integer> ar = new HashMap<>();
+        int low = 0, res = -1;
+
+        for (int high = 0; high < n; high++) {
+
+            char c = arr.charAt(high);
+            ar.put(c,ar.getOrDefault(c,0)+1);
+
+            while(ar.size()>k)
+            {
+               char leftChar = arr.charAt(low);
+                ar.put(leftChar,ar.get(leftChar)-1);
+                if (ar.get(leftChar)==0)
+                {
+                    ar.remove(leftChar);
+                }
+                low++;
+            }
+            if (ar.size()==k)
+            {
+                res = Math.max(res,high-low+1);
+            }
+
+
+
+
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        String arr = "aabbc";
+        int k = 2;
+        System.out.println(LongestSubString(arr, k));
+
+    }
+}
